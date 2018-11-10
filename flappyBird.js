4f7bab29ac7cc4489e51923aceaf4bafb7974bf8
Aditya Singh <adityasingh1297@gmail.com>
2018-11-11 00:14:45 +0530

    Flappy bird with evil morty

---
+++ flappyBird.js
@@ -0,0 +1,99 @@
+// alert("hey")
+var cvs = document.getElementById("canvas");
+var ctx = cvs.getContext("2d");
+
+//load images
+
+var bird = new Image();
+var bg = new Image();
+var fg = new Image();
+var pipeNorth = new Image();
+var pipeSouth = new Image();
+
+bird.src = "images/evilmorty1.jpeg";
+bg.src ="images/bg.png" ;
+fg.src ="images/fg.png" ;
+pipeNorth.src ="images/pipeNorth.png" ;
+pipeSouth.src ="images/pipeSouth.png" ;
+
+var gap = 100;
+var constant = pipeNorth.height +gap;
+
+
+var bX = 10;
+var bY = 150;
+
+var gravity =1.5;
+var score =0;
+
+
+//on key down
+
+document.addEventListener("keydown",moveUp);
+
+function moveUp(){
+	bY -=30; 
+}
+
+
+//pipe coordinates
+
+var pipe = [];
+
+pipe[0] = {
+	x :cvs.width,
+	y : 0
+};
+
+
+//draw images
+
+function draw(){
+	ctx.drawImage(bg,0,0);
+
+	for(var i =0;i<pipe.length;i++){
+		ctx.drawImage(pipeNorth,pipe[i].x,pipe[i].y);
+		ctx.drawImage(pipeSouth,pipe[i].x,pipe[i].y + constant);	
+
+		pipe[i].x--;
+
+		if(pipe[i].x ==150){
+			pipe.push({
+				x : cvs.width,
+				y : Math.floor(Math.random()*pipeNorth.height)-pipeNorth.height
+			});
+		}
+
+		if(bX>=pipe[i].x && bX+bird.width<=pipe[i].x+pipeNorth.width && (bY<=pipe[i].y+pipeNorth.height || bY+bird.height>=pipe[i].y+constant) || bY+bird.height>=cvs.height-fg.height)
+		{
+			location.reload();
+		}
+
+		if(pipe[i].x==5)
+		{
+			score++;
+		}
+
+
+	}
+
+
+	
+
+	ctx.drawImage(fg,0,cvs.height - fg.height);
+
+	ctx.drawImage(bird,bX,bY);
+
+	bY += gravity;
+
+	ctx.fillStyle = "#000";
+	ctx.font = "20px Verdana";
+	ctx.fillText("Score :"+score,10,cvs.height-20);
+
+	requestAnimationFrame(draw);
+
+
+}
+
+
+draw();
